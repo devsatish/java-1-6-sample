0bcbd9b9e88c7c1b9e2099dcfdf4f2c6653560b5
Second Agent <123514301+second-agent[bot]@users.noreply.github.com>
2024-06-26 23:54:21 +0000

    edit_file
    # Path: step_5/for_each/edit_file
    # UUID: 91ce4753-c5ba-4cb8-bb17-e984502b1852
    
    # Changed files:
    src/Deck.java
    - 42cd38c4de23a8f9bf89f6255a3411a759b05dbc

--- src/Deck.java
+++ src/Deck.java
@@ -1,5 +1,15 @@
 /**
- * @author scrub
+ * This class represents a deck of cards.
+ * It provides functionalities to create, shuffle, and manipulate a deck of cards.
+ * 
+ * New Java 1.8 features used:
+ * - Streams and lambda expressions for concise and readable code.
+ * - Collectors for joining strings.
+ * - Method references for cleaner code.
+ * 
+ * Note: This class is not thread-safe.
+ * 
+ * @version 1.8
  */
 import java.util.ArrayList;
 import java.util.Random;
@@ -10,6 +20,11 @@ public class Deck {
 
     private ArrayList<Card> deck;
 
+    /**
+     * Copy constructor for creating a deep copy of an existing deck.
+     * 
+     * @param original the original deck to copy
+     */
     Deck(Deck original) {
         this.deck = new ArrayList<>();
 
@@ -19,6 +34,9 @@ public class Deck {
         });
     }
 
+    /**
+     * Default constructor for creating a standard deck of 52 cards.
+     */
     Deck() {
         this.deck = new ArrayList<>();
 
@@ -29,6 +47,11 @@ public class Deck {
         );
     }
 
+    /**
+     * Returns a string representation of the deck.
+     * 
+     * @return a string representation of the deck
+     */
     @Override
     public String toString() {
         return deck.stream()
@@ -36,11 +59,18 @@ public class Deck {
                    .collect(Collectors.joining());
     }
 
+    /**
+     * Returns the total number of cards in the deck.
+     * 
+     * @return the total number of cards
+     */
     public int getTotalCards() {
         return deck.size();
     }
 
-    /** Toma 100 pares de cartas y los cambia de lugar para revolver el deck. **/
+    /**
+     * Shuffles the deck by swapping 100 pairs of cards.
+     */
     public void shuffle() {
         Random generator = new Random();
 
@@ -54,20 +84,40 @@ public class Deck {
         });
     }
 
+    /**
+     * Deals the top card from the deck.
+     * 
+     * @return the top card
+     */
     public Card dealCard() {
         return deck.remove(0);
     }
 
+    /**
+     * Deals a random card from the deck.
+     * 
+     * @return a random card
+     */
     public Card dealRandomCard() {
         Random generator = new Random();
         int index = generator.nextInt(this.deck.size());
         return deck.remove(index);
     }
 
+    /**
+     * Removes a specific card from the deck.
+     * 
+     * @param card the card to remove
+     */
     public void removeCard(Card card) {
         deck.removeIf(c -> c.getRank() == card.getRank() && c.getSuit() == card.getSuit());
     }
 
+    /**
+     * Removes multiple cards from the deck based on their string representations.
+     * 
+     * @param c an array of string representations of the cards to remove
+     */
     public void removeCards(String[] c) {
         for (String cardStr : c) {
             Card temp = new Card(cardStr);
@@ -75,22 +125,47 @@ public class Deck {
         }
     }
 
+    /**
+     * Returns the card at the specified index.
+     * 
+     * @param index the index of the card to return
+     * @return the card at the specified index
+     */
     public Card getCard(int index) {
         return deck.get(index);
     }
 
+    /**
+     * Adds a card to the deck.
+     * 
+     * @param c the card to add
+     */
     public void addCard(Card c) {
         this.deck.add(c);
     }
 
+    /**
+     * Checks if the deck contains a specific card.
+     * 
+     * @param card the card to check for
+     * @return true if the deck contains the card, false otherwise
+     */
     public boolean containsCard(Card card) {
         return deck.contains(card);
     }
 
+    /**
+     * Converts the deck to an array of cards.
+     * 
+     * @return an array of cards
+     */
     public Card[] toArray() {
         return deck.toArray(new Card[0]);
     }
 
+    /**
+     * Clears the deck of all cards.
+     */
     public void clearDeck() {
         this.deck.clear();
     }
