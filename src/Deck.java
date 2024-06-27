95e4ccc7ff334a341c0b37583b39b6cbd7bdff8f
Second Agent <123514301+second-agent[bot]@users.noreply.github.com>
2024-06-27 03:26:04 +0000

    edit_file
    # Path: step_1/for_each/edit_file
    # UUID: 0c81e420-1d15-45b4-aa69-b81cec060b4b
    
    # Changed files:
    src/Deck.java

--- src/Deck.java
+++ src/Deck.java
@@ -1,113 +1,97 @@
-/**
- * @author scrub
- */
 import java.util.ArrayList;
 import java.util.Random;
 
-public class Deck{
-    
+public class Deck {
+
     private ArrayList<Card> deck;
-    
-    Deck(Deck original){
-        this.deck = new ArrayList<Card>();
-        
-        for ( int i=0 ; i<original.getTotalCards(); i++){
-            Card card_temp = original.getCard(i);
-            Card deepCopy = new Card(card_temp.getRank(), card_temp.getSuit());
+
+    Deck(Deck original) {
+        this.deck = new ArrayList<>();
+
+        for (int i = 0; i < original.getTotalCards(); i++) {
+            var cardTemp = original.getCard(i);
+            var deepCopy = new Card(cardTemp.getRank(), cardTemp.getSuit());
             this.deck.add(deepCopy);
         }
     }
 
-    Deck(){
-         
-        this.deck = new ArrayList<Card>();
+    Deck() {
+        this.deck = new ArrayList<>();
 
-        for (int suit=0; suit<4; suit++){
-            for (int rank=0; rank<13; rank++){
-                this.deck.add(new Card(rank,suit));
+        for (int suit = 0; suit < 4; suit++) {
+            for (int rank = 0; rank < 13; rank++) {
+                this.deck.add(new Card(rank, suit));
             }
         }
     }
-    
 
-    public @Override String toString(){
-          String strDeck = "";
-          for(int i=0; i<deck.size(); i++){
-              strDeck += deck.get(i).toString(); 
-          }
-          return strDeck;
+    @Override
+    public String toString() {
+        var strDeck = new StringBuilder();
+        for (var card : deck) {
+            strDeck.append(card.toString());
+        }
+        return strDeck.toString();
     }
-        
-    public int getTotalCards(){
+
+    public int getTotalCards() {
         return deck.size();
     }
-    
-    /** Toma 100 pares de cartas y los cambia de lugar para revolver el deck. **/
-    public void shuffle(){
-        
-        int index_1, index_2;
-        Random generator = new Random();
-        Card temp;
-        
-        for (int i=0; i<100; i++){
-            index_1 = generator.nextInt(this.deck.size());
-            index_2 = generator.nextInt(this.deck.size());
 
-            temp = deck.get(index_2);
-            deck.set(index_2, deck.get(index_1));
-            deck.set(index_1, temp);
-        }    
+    /** Toma 100 pares de cartas y los cambia de lugar para revolver el deck. **/
+    public void shuffle() {
+        var generator = new Random();
+
+        for (int i = 0; i < 100; i++) {
+            var index1 = generator.nextInt(this.deck.size());
+            var index2 = generator.nextInt(this.deck.size());
+
+            var temp = deck.get(index2);
+            deck.set(index2, deck.get(index1));
+            deck.set(index1, temp);
+        }
     }
-    
-    public Card dealCard(){
-        return (Card)deck.remove(0);
+
+    public Card dealCard() {
+        return deck.remove(0);
     }
-    
-    public Card dealRandomCard(){
-        Random generator = new Random();
-        int index = generator.nextInt(this.deck.size());
-        Card randomCard = deck.get(index);
+
+    public Card dealRandomCard() {
+        var generator = new Random();
+        var index = generator.nextInt(this.deck.size());
+        var randomCard = deck.get(index);
         deck.remove(index);
         return randomCard;
     }
-    
-    public void removeCard(Card card){
-        for(int i=0; i<deck.size(); i++){
-            if(card.getRank()== deck.get(i).getRank() && card.getSuit() == deck.get(i).getSuit()){
-                deck.remove(i);
-            }
+
+    public void removeCard(Card card) {
+        deck.removeIf(c -> c.getRank() == card.getRank() && c.getSuit() == card.getSuit());
+    }
+
+    public void removeCards(String[] c) {
+        for (var cardStr : c) {
+            var temp = new Card(cardStr);
+            deck.removeIf(card -> card.getRank() == temp.getRank() && card.getSuit() == temp.getSuit());
         }
     }
-    
-    public void removeCards(String[] c){
-        Card temp;
-        for(int i=0; i<c.length; i++){
-            temp = new Card(c[i]);
-            for(int r=0; r<deck.size(); r++){
-                if(temp.getRank()== deck.get(r).getRank() && temp.getSuit() == deck.get(r).getSuit()){
-                    deck.remove(r);
-                }
-            }
-        }
+
+    public Card getCard(int index) {
+        return deck.get(index);
     }
-    
-    public Card getCard(int index){
-        return (Card)deck.get(index);
-    }
-    public void addCard(Card c){
+
+    public void addCard(Card c) {
         this.deck.add(c);
     }
-    
-    public boolean containsCard(Card card){
+
+    public boolean containsCard(Card card) {
         return deck.contains(card);
     }
-    
-    public Card[] toArray(){
-        return (Card[])deck.toArray();  
+
+    public Card[] toArray() {
+        return deck.toArray(new Card[0]);
     }
-    
-    public void clearDeck(){
+
+    public void clearDeck() {
         this.deck.clear();
     }
-    
 }
