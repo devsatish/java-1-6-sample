ada7273f4b8f38aa9cb43c6e0cd011da08a2bdf5
Second Agent <123514301+second-agent[bot]@users.noreply.github.com>
2024-06-27 03:26:02 +0000

    edit_file
    # Path: step_1/for_each/edit_file
    # UUID: e7d094b2-535c-4c67-8422-b68e588e6bb5
    
    # Changed files:
    src/Card.java

--- src/Card.java
+++ src/Card.java
@@ -1,50 +1,48 @@
 /**
  * @author scrub
  */
-public class Card{
+public class Card {
     private int rank, suit;
 
-    private static String[] suits = { "s", "h", "c", "d" };
-    private static String[] ranks  = { "A", "2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K" };
- 
-    Card(int rank, int suit){
-        this.rank = rank;
-        this.suit = suit;     
-    }
-    
-    Card(String card){
-        
-        String cardRank = card.substring(0, 1);
-        String cardSuit = card.substring(1, 2);
-        //System.out.println(cardRank+cardSuit);
+    private static final String[] suits = { "s", "h", "c", "d" };
+    private static final String[] ranks = { "A", "2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K" };
 
-        for (int r=0; r<13; r++){
-            if(cardRank.equals(ranks[r])){
+    Card(int rank, int suit) {
+        this.rank = rank;
+        this.suit = suit;
+    }
+
+    Card(String card) {
+        var cardRank = card.substring(0, 1);
+        var cardSuit = card.substring(1, 2);
+
+        for (var r = 0; r < 13; r++) {
+            if (cardRank.equals(ranks[r])) {
                 this.rank = r;
             }
         }
-        
-        for (int s=0; s<4; s++){
-            if(cardSuit.equals(suits[s])){
+
+        for (var s = 0; s < 4; s++) {
+            if (cardSuit.equals(suits[s])) {
                 this.suit = s;
             }
         }
     }
-    
-    public static String rankAsString(int n){
+
+    public static String rankAsString(int n) {
         return ranks[n];
     }
 
-    public @Override String toString(){
-          return ranks[rank] + "" + suits[suit];
+    @Override
+    public String toString() {
+        return ranks[rank] + "" + suits[suit];
     }
 
-    public int getRank(){
-         return rank;
+    public int getRank() {
+        return rank;
     }
 
-    public int getSuit(){
+    public int getSuit() {
         return suit;
     }
-    
 }
